87dce33598eac69931131c8118c4fe4a18d3a081
pkumv1 <pkumv1@gmail.com>
2024-09-15 12:28:44 +0530

    started experiments with openai

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
 from setuptools import find_packages, setup
 
 
-setup()
+setup(
+    name="mcq generator",
+    version="0.0.1",
+    author="pavan",
+    author_email="pkumv1@gmail.com",
+    install_requires=[
+        "openai",
+        "langchain",
+        "pandas",
+        "numpy",
+        "seaborn",
+        "matplotlib",
+        "streamlit",
+        "pyPDF",
+        "python-dotenv",
+    ],
+    packages=find_packages(),
+)
